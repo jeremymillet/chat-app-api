c596f629c9a1b2c626736c0ebae3a2aa2e0d598d
jeremymillet <jeremy.millet37@gmail.com>
2025-01-17 15:06:27 +0100

    creation de la route get conversation et modification du services conversation

--- src/main/java/com/chatapp/api/service/ConversationService.java
+++ src/main/java/com/chatapp/api/service/ConversationService.java
@@ -1,8 +1,11 @@
 package com.chatapp.api.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.chatapp.api.dto.ConversationDTO;
 import com.chatapp.api.entity.Conversation;
 import com.chatapp.api.repository.ConversationRepository;
 
@@ -12,13 +15,16 @@ public class ConversationService {
     @Autowired
     private ConversationRepository conversationRepository;
 
-    public Conversation getOrCreateConversation(Long friendshipId) {
-        return conversationRepository.findByFriendshipId(friendshipId)
-                .orElseGet(() -> {
-                    // Si la conversation n'existe pas, créer une nouvelle conversation
-                    Conversation conversation = new Conversation();
-                    conversation.setFriendshipId(friendshipId);
-                    return conversationRepository.save(conversation);
-                });
+    public ConversationDTO getOrCreateConversation(Long friendshipId) {
+        Optional<Conversation> optionalConversation = conversationRepository.findByFriendshipId(friendshipId);
+
+        Conversation conversation = optionalConversation.orElseGet(() -> {
+            // Si la conversation n'existe pas, créer une nouvelle
+            Conversation newConversation = new Conversation();
+            newConversation.setFriendshipId(friendshipId);
+            return conversationRepository.save(newConversation);
+        });
+
+        return new ConversationDTO(conversation.getId(), conversation.getFriendshipId());
     }
 }
