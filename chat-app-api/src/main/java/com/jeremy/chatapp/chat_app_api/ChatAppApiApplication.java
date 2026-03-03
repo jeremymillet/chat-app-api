4cab78b2c3ff587a38eda8632d0bdafa07e1a305
jeremymillet <jeremy.millet37@gmail.com>
2026-03-03 17:17:47 +0100

    add main application files and base configuration

--- chat-app-api/src/main/java/com/jeremy/chatapp/chat_app_api/ChatAppApiApplication.java
+++ chat-app-api/src/main/java/com/jeremy/chatapp/chat_app_api/ChatAppApiApplication.java
@@ -2,10 +2,11 @@ package com.jeremy.chatapp.chat_app_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication()
 public class ChatAppApiApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChatAppApiApplication.class, args);
 	}
